b59f2e6891c2ec95fe154cc6c5c7aa95790d799a
Nikita Belov <nik.belov@yahoo.com>
2017-06-20 23:08:37 +0300

    Second commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            ViewBag.Greeting = "Hi";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //TODO: email to organaiser
+
+            return View("Thanks", guestResponse);
+        }
+    }
+}
